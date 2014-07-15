895e31f2f7c235ea33982cacd5483292658d3635
lbybee <leland.bybee@gmail.com>
2014-07-15 17:50:38 -0500

    alpha posterior

--- gamma_mcmc.py
+++ gamma_mcmc.py
@@ -6,6 +6,27 @@ def processWords(word):
     return
 
 
+def alphaPost(alpha_n1, gamma_i, delta2, a2 theta_dit, I, d_it):
+    """generates the full conditional for alpha_it
+    
+    delta2: scalar
+    a2: scalar
+    I: NxN identity matrix
+    d_it: scalar, the number of documents for a given node in a given period
+    alpha_it: NxK matrix of proportions for each node.  Each row is a node and each column is a proportion
+    gamma_i: Nx1 vector, each element is a weight for that node
+    theta_dit: DxK matrix of draws for each document.  Each row is the document and each column is the topic
+
+    """
+
+    # NOTE: multiplying lambda_ait * ... may not have the desired effect, this likely will need to be
+    # fixed in testing
+
+    lambda_ait = linalg.inv(1 / delta2 * I + d_it / a2 * I)
+    mu_ait = lambda_ait * (1 / delta2 * I * dot(alpha_it * gamma_i) + d_it / a2 * I *  sum(theta_dit))
+    return random.multivariate_normal(mu_ait, lambda_ait)
+
+
 def fullRun(N, K, T, V, iterations, graph, xi2, delta2): 
 
     # initalize values
