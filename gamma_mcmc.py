47f082f8d2706dbeed198fa1f6285cb6dcd85411
lbybee <leland.bybee@gmail.com>
2014-07-15 22:03:05 -0500

    posterior beta

--- gamma_mcmc.py
+++ gamma_mcmc.py
@@ -6,16 +6,17 @@ def processWords(word):
     return
 
 
-def alphaPost(alpha_n1, gamma_i, delta2, a2 theta_dit, I, d_it):
+def alphaPost(alpha_n1, gamma_i, delta2, a2 theta_it, I, d_it):
     """generates the full conditional for alpha_it
     
     delta2: scalar
     a2: scalar
-    I: NxN identity matrix
+    I: KxK identity matrix
     d_it: scalar, the number of documents for a given node in a given period
     alpha_it: NxK matrix of proportions for each node.  Each row is a node and each column is a proportion
     gamma_i: Nx1 vector, each element is a weight for that node
-    theta_dit: DxK matrix of draws for each document.  Each row is the document and each column is the topic
+    dot(alpha_it, gamma_i): Kx1 vector
+    theta_it: DxK matrix of draws for each document.  Each row is the document and each column is the topic
 
     """
 
@@ -23,8 +24,25 @@ def alphaPost(alpha_n1, gamma_i, delta2, a2 theta_dit, I, d_it):
     # fixed in testing
 
     lambda_ait = linalg.inv(1 / delta2 * I + d_it / a2 * I)
-    mu_ait = lambda_ait * (1 / delta2 * I * dot(alpha_it * gamma_i) + d_it / a2 * I *  sum(theta_dit))
+    mu_ait = lambda_ait * (1 / delta2 * I * dot(alpha_it, gamma_i) + d_it / a2 * I *  sum(theta_dit))
     return random.multivariate_normal(mu_ait, lambda_ait)
+
+
+def betaPost(beta_n1, sigma2, b2, phi_t, I, k):
+    """generates the full conditional for beta_t
+
+    sigma2: scalar
+    b2: scalar
+    I: VxV identity matrix
+    k: scalar, the number of topics
+    beta_n1: Vx1 vector
+    phi_t: KxV matrix of proportions for each topic for each word.
+
+    """
+
+    lambda_bit = linalg.inv(1 / sigma2 * I + k / b2 * I)
+    mu_bit = lambda_bit * (1 / sigma2 * I * beta_n1 + k / b2 * I * sum(phi_t))
+    return random.multivariate_normal(mu_bit, lambda_bit)
 
 
 def fullRun(N, K, T, V, iterations, graph, xi2, delta2): 
