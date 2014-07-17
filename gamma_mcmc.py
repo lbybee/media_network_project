fa691c42fdd6324c2ffa7ee656843d005226943b
Leland Bybee <leland.bybee@gmail.com>
2014-07-16 23:09:21 -0500

    Update gamma_mcmc.py

--- gamma_mcmc.py
+++ gamma_mcmc.py
@@ -75,12 +75,19 @@ def fullRun(N, K, T, V, iterations, graph, xi2, delta2):
     beta = array([[1. for v in range(V)] for t in range(T)])
     phi = array([[[1. for v in range(V)] for k in range(K)] for t in range(T)])
     theta = array([[[[1. for d in range(len(docs))] for docs in period] for period in node] for node in graph])
+    
+    # initalize storage
+    gamma_s = array([[[1. / N for i in range(N)] for i in range(N)] for s in range(S)])
+    alpha_s = array([[[[1. for k in range(K)] for t in range(T)] for i in range(N)] for s in range(S)])
+    beta_s = array([[[1. for v in range(V)] for t in range(T)] for s in range(S)])
+    phi_s = array([[[[1. for v in range(V)] for k in range(K)] for t in range(T)] for s in range(S)])
+    theta_s = array([[[[[1. for d in range(len(docs))] for docs in period] for period in node] for node in graph] for s in range(S)])
+    
+    
 
     for j in iterations:
+        alpha_n = genPevAlpha(alpha)
         for gamma_i, alpha_i, theta_i in zip(gamma, alpha, theta):
-            # Gibbs sampling for gamma i
-            lambda_gi = linalg.inv(linalg.inv(xi2) + transpose(alpha_n1) * linalg.inv(delta2) * alpha_n1)
-            mu_gi = lambda_gi * (linalg.inv(xi2) * eta + transpose(alpha_n1) * linalg.inv(delta2) * alpha_i)
-            gamma_i = random.multivariate_normal(mu_gi, lambda_gi)
-            for alpha_it, 
+            
+            
 
