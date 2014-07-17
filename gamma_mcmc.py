2ed20f49ec14432456c0c6b00e7fcc94bd0a5a32
lbybee <leland.bybee@gmail.com>
2014-07-16 19:38:21 -0500

    add gamma code

--- gamma_mcmc.py
+++ gamma_mcmc.py
@@ -13,18 +13,17 @@ def alphaPost(alpha_n1, gamma_i, delta2, a2 theta_it, I, d_it):
     a2: scalar
     I: KxK identity matrix
     d_it: scalar, the number of documents for a given node in a given period
-    alpha_it: NxK matrix of proportions for each node.  Each row is a node and each column is a proportion
+    alpha_n1: NxK matrix of proportions for each node.  Each row is a node and each column is a proportion
     gamma_i: Nx1 vector, each element is a weight for that node
-    dot(alpha_it, gamma_i): Kx1 vector
+    dot(alpha_n1, gamma_i): Kx1 vector
     theta_it: DxK matrix of draws for each document.  Each row is the document and each column is the topic
+    mu_ait: Kx1 vector
 
     """
 
-    # NOTE: multiplying lambda_ait * ... may not have the desired effect, this likely will need to be
-    # fixed in testing
 
     lambda_ait = linalg.inv(1 / delta2 * I + d_it / a2 * I)
-    mu_ait = lambda_ait * (1 / delta2 * I * dot(alpha_it, gamma_i) + d_it / a2 * I *  sum(theta_dit))
+    mu_ait = dot(lambda_ait, (1 / delta2 * I * dot(alpha_n1, gamma_i) + d_it / a2 * I *  sum(theta_dit)))
     return random.multivariate_normal(mu_ait, lambda_ait)
 
 
@@ -37,23 +36,34 @@ def betaPost(beta_n1, sigma2, b2, phi_t, I, k):
     k: scalar, the number of topics
     beta_n1: Vx1 vector
     phi_t: KxV matrix of proportions for each topic for each word.
-
+    mu_bit: Vx1 vector
+    
     """
 
     lambda_bit = linalg.inv(1 / sigma2 * I + k / b2 * I)
-    mu_bit = lambda_bit * (1 / sigma2 * I * beta_n1 + k / b2 * I * sum(phi_t))
+    mu_bit = dot(lambda_bit, (1 / sigma2 * I * beta_n1 + k / b2 * I * sum(phi_t)))
     return random.multivariate_normal(mu_bit, lambda_bit)
 
 
 def gammaPost(alpha_i, alpha_n, xi2, mu, delta2, I):
     """generates the full conditional for gamma_i
 
-    
+    xi2: scalar
+    delta2: scalar
+    I: KxK identity matrix
+    alpha_n: Tx(NXK) matrix of alpha_it-1 matricies.  alpha_it-1 is an NxK matrix of proportions for each node.
+    Each row is a node and each column is a proportion
+    alpha_i: Tx(Kx1) matrix of alpha_it values.  alpha_it is a vector of the topic proportions for the ith node.
+    gamma_i: Nx1 vector, each element is a weight for that node
+    dot(sum(alpha_n), sum(alpha_n)): KxK matrix
+    dot(sum(alpha_n), sum(alpha_i)): Nx1 vector
+    eta: Nx1 vector, the prior for gamma_i
+    mu_gi: Nx1 vector
 
     """
 
-    lambda_gi = linalg.inv(1 / xi2 * I + dot(sum(alpha_n), sum(alpha_n) / delta2))
-    mu_gi = lambda_gi * (eta / xi2 + dot(sum(alpha_n), sum(alpha_i)) / delta2)
+    lambda_gi = linalg.inv(1 / xi2 * I + dot(sum(alpha_n), sum(alpha_n)) / delta2)
+    mu_gi = dot(lambda_gi, (eta / xi2 + dot(sum(alpha_n), sum(alpha_i)) / delta2))
     return random.multivariate_normal(mu_gi, lambda_gi)
 
 
