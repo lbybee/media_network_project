1dab9ae3e94f2e0b7f0d64137decd980482d6ce9
lbybee <leland.bybee@gmail.com>
2014-07-23 18:58:08 -0500

    fix paper and fix text cleaner

--- text_cleaner.py
+++ text_cleaner.py
@@ -25,7 +25,7 @@ def addWordTDict(w, date, node, data_dict):
     return data_dict
 
 
-def extractData(csv_f):
+def extractData(csv_f, month_date):
     """extracts the data from the csv"""
 
     rx = re.compile("\W+")
@@ -40,14 +40,18 @@ def extractData(csv_f):
         text = row[2]
         node = row[1]
         dateb = row[0].split(" ")
-        period = dateb[1] + " " + dateb[2] + " " + dateb[5]
+        if month_date:
+            period = dateb[2] + " " + dateb[5]
+        else:
+            period = dateb[1] + " " + dateb[2] + " " + dateb[5]
         text = text.lower()
-        text = rx.sub(" ", t).strip()
+        text = rx.sub(" ", text).strip()
         for w in word_tokenize(text):
-            w = PorterStemmer.stem_word(w)
+            w = PorterStemmer().stem_word(w)
             if w not in stopwords.words("english"):
                 data_dict = addWordTDict(w, period, node, data_dict)
-        print (i * 100.) / data_ln, datetime.now() - t_1
+        print (i * 100.) / reader_ln, datetime.now() - t_1
+    return data_dict
 
 
 def getVocab(data_dict):
@@ -78,3 +82,11 @@ def writeDictionary(data_dict, csv_f, vocab):
                 if v in data_dict[d][n]:
                     writer.writerow([v, d, n, data_dict[d][n][v]])
         print (i * 100.) / data_ln, datetime.now() - t_1, "csv"
+
+
+def fullRun(i_csv_f, o_csv_f, month_date=False):
+    """does a full run"""
+
+    data_dict = extractData(i_csv_f, month_date)
+    vocab = getVocab(data_dict)
+    writeDictionary(data_dict, o_csv_f, vocab)
