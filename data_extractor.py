4da1858e518b2549326fb411cd27f3067a33c551
lbybee <leland.bybee@gmail.com>
2014-07-29 10:23:16 -0500

    remove bad users

--- data_extractor.py
+++ data_extractor.py
@@ -1,5 +1,6 @@
 from datetime import datetime
 from pymongo import MongoClient
+import cPickle
 import sys
 import csv
 import os
@@ -15,13 +16,18 @@ t_1 = datetime.now()
 i = 0
 
 os.chdir(sys.argv[2])
+
+# load user ids
+user_ids = cPickle.load(open(sys.argv[3], "rb"))
+
 for obs in collection.find():
     f_name = open(sys.argv[1], "ab")
     writer = csv.writer(f_name)
-    text = obs["text"].encode("utf-8")
-    agent = obs["user"]["screen_name"]
-    date = obs["created_at"]
-    writer.writerow([date, agent, text])
-    f_name.close()
+    if obs["user"]["id"] in user_ids:
+        text = obs["text"].encode("utf-8")
+        agent = obs["user"]["screen_name"]
+        date = obs["created_at"]
+        writer.writerow([date, agent, text])
+        f_name.close()
     print (i * 100.) / ln, datetime.now() - t_1, i
     i += 1
