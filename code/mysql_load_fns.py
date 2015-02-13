1622437f81a17f6f84d0220ac6d55da4608354de
lbybee <leland.bybee@gmail.com>
2015-02-13 14:23:27 -0600

    update formating

--- code/mysql_load_fns.py
+++ code/mysql_load_fns.py
@@ -8,50 +8,50 @@ def initTwitterTables(host, user, passwd, db, tweet_tab, user_tab):
     rdb = MySQLdb.connect(host=host, user=user, passwd=passwd, db=db)
     cursor = rdb.cursor()
 
-    tweet_str = """CREATE TABLE '%s' (
-                   TID INT,
-                   YEAR INT,
-                   MONTH INT,
-                   DAY INT,
-                   HOUR INT,
-                   MINUTE INT,
-                   SECOND INT,
+    tweet_str = """CREATE TABLE %s (
+                   TID INT(25),
+                   YEAR INT(4),
+                   MONTH INT(2),
+                   DAY INT(2),
+                   HOUR INT(2),
+                   MINUTE INT(2),
+                   SECOND INT(2),
                    IN_REPLY_TO_SCREEN_NAME VARCHAR(15),
-                   IN_REPLY_TO_STATUS_ID INT,
-                   IN_REPLY_TO_USER_ID INT,
+                   IN_REPLY_TO_STATUS_ID INT(25),
+                   IN_REPLY_TO_USER_ID INT(25),
                    LANG VARCHAR(10),
-                   POSSIBLY_SENSITVE INT,
-                   RETWEET_COUNT INT,
-                   RETWEETED INT,
+                   POSSIBLY_SENSITVE INT(1),
+                   RETWEET_COUNT INT(10),
+                   RETWEETED INT(1),
                    SOURCE VARCHAR(100),
                    TEXT VARCHAR(140),
-                   UID INT )""" % tweet_tab
+                   UID INT(25) )""" % tweet_tab
 
-    user_str = """CREATE TABLE '%s' (
-                  UID INT,
-                  YEAR INT,
-                  MONTH INT,
-                  DAY INT,
-                  HOUR INT,
-                  MINUTE INT,
-                  SECOND INT,
-                  DEFAULT_PROFILE INT,
+    user_str = """CREATE TABLE %s (
+                  UID INT(25),
+                  YEAR INT(4),
+                  MONTH INT(2),
+                  DAY INT(2),
+                  HOUR INT(2),
+                  MINUTE INT(2),
+                  SECOND INT(2),
+                  DEFAULT_PROFILE INT(1),
                   DESCRIPTION VARCHAR(160),
-                  DEFAULT_PROFIlE_IMAGE INT,
-                  FAVOURITES_COUNT INT,
-                  FOLLOWERS_COUNT INT,
-                  FRIEND_COUNT INT,
-                  GEO_ENABLED INT,
+                  DEFAULT_PROFIlE_IMAGE INT(1),
+                  FAVOURITES_COUNT INT(10),
+                  FOLLOWERS_COUNT INT(10),
+                  FRIEND_COUNT INT(10),
+                  GEO_ENABLED INT(1),
                   LANG VARCHAR(10),
-                  LISTED_COUNT INT,
+                  LISTED_COUNT INT(10),
                   LOCATION VARCHAR(50),
                   NAME VARCHAR(20),
-                  PROTECTED INT,
+                  PROTECTED INT(1),
                   SCREEN_NAME VARCHAR(15),
-                  STATUSES_COUNT INT,
+                  STATUSES_COUNT INT(10),
                   URL VARCHAR(100),
-                  UTC_OFFSET INT,
-                  VERIFIED INT )""" % user_tab
+                  UTC_OFFSET INT(10),
+                  VERIFIED INT(1) )""" % user_tab
 
     cursor.execute(tweet_str)
     print "hi"
